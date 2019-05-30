cea5f4623471069558595a3f69b709832a6a3dbc
Chi Voong <chi.voong@skybettingandgaming.com>
2019-05-30 10:13:14 +0100

    change controller to accept form instead of rest

--- src/main/kotlin/com/skybet/cv/controller/PersonController.kt
+++ src/main/kotlin/com/skybet/cv/controller/PersonController.kt
@@ -2,18 +2,20 @@ package com.skybet.cv.controller
 
 import com.skybet.cv.data.Person
 import com.skybet.cv.service.PersonService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.*
 
-@RestController
+@Controller
 class PersonController (val personService: PersonService){
 
 
     //receives a list of people from a html form and puts them into a database
-    @PostMapping("update")
-    fun update(@RequestBody persons:List<Person>) = personService.update(persons)
+    @GetMapping("update")
+    fun update(@RequestParam("people[][firstname]") firstnames:List<String>,
+               @RequestParam("people[][surname]") lastnames:List<String>) {
+        val persons = firstnames.zip(lastnames).map{Person(it.first,it.second)}
+        personService.update(persons)
+    }
 
     @GetMapping("load")
     fun load() = personService.load()
