e7d73d93bfc85968c6cfeb36e310b066447ff3b2
Chi Voong <chi.voong@skybettingandgaming.com>
2019-05-30 15:25:17 +0100

    Added thymeleaf

---
+++ src/main/kotlin/com/skybet/cv/config/ThymeleafConfig.kt
@@ -0,0 +1,5 @@
+package com.skybet.cv.config
+
+class ThymeleafConfig {
+
+}
--- src/main/kotlin/com/skybet/cv/controller/PersonController.kt
+++ src/main/kotlin/com/skybet/cv/controller/PersonController.kt
@@ -2,12 +2,18 @@ package com.skybet.cv.controller
 
 import com.skybet.cv.data.Person
 import com.skybet.cv.service.PersonService
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
 @Controller
 class PersonController (val personService: PersonService){
 
+    @Value("\${welcome.message}")
+    private lateinit var message:String
+
+    private val tasks = listOf("a","b","c","d","e")
 
     //receives a list of people from a html form and puts them into a database
     @GetMapping("update")
@@ -19,4 +25,17 @@ class PersonController (val personService: PersonService){
 
     @GetMapping("load")
     fun load() = personService.load()
+
+    @GetMapping("hello")
+    fun welcomeExample(@RequestParam(name = "name", required = false, defaultValue = "") name:String,  model:Model):String {
+        model.addAttribute("message", name)
+        return "welcome" //view
+    }
+
+    @GetMapping("/")
+    fun welcome(model:Model):String {
+        model.addAttribute("message",message)
+        model.addAttribute("tasks",tasks)
+        return "welcome"
+    }
 }
